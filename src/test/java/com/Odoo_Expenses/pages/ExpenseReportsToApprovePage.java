e47d121a72408b4dad45a8ccf69390fec542538a
mussie tewolde <mgbre2@yahoo.com>
2019-02-08 11:52:24 -0500

    mussie

--- src/test/java/com/Odoo_Expenses/pages/ExpenseReportsToApprovePage.java
+++ src/test/java/com/Odoo_Expenses/pages/ExpenseReportsToApprovePage.java
@@ -18,14 +18,33 @@ public class ExpenseReportsToApprovePage {
     public WebElement ExpenseReportsToApp;
 
 
-    @FindBy(xpath = "//button[contains(text(),'Create']")
+    @FindBy(xpath = "//button[@accesskey='c']")
+    //@FindBy(linkText = "Create")
     public WebElement create;
 
-    @FindBy(xpath = "(//button[contains(text(),'Import']")
+    //@FindBy(xpath = "(//button[contains(text(),'Import']")
+    //@FindBy(xpath = "(//button [@type='button'])[22]")
+    @FindBy(xpath = "//button[@class='.Import']")
     public WebElement Impo;
 
     @FindBy(className = "o_searchview_input")
     public WebElement Search;
+
+    @FindBy(xpath = "//button[@accesskey='s']")
+    public WebElement save;
+
+    @FindBy(xpath = "(//input[@type='text'])[4]")
+    public WebElement summarybox;
+
+    @FindBy(xpath = "(//input[@type='text'])[5]")
+    public WebElement eployee;
+    @FindBy(xpath = "//button [@class='btn btn-sm btn-default'][1]")
+    public WebElement approve;
+
+
+    @FindBy(xpath = "//button [@class='btn btn-sm btn-default'][5]")
+    public WebElement refused;
+
 
     public void Expense(){
         expense.click();
--- src/test/java/com/Odoo_Expenses/tests/smoke_tests/ExpenseReportToApproveTest.java
+++ src/test/java/com/Odoo_Expenses/tests/smoke_tests/ExpenseReportToApproveTest.java
@@ -4,16 +4,17 @@ import com.Odoo_Expenses.utilities.ApplicationConstants;
 import com.Odoo_Expenses.utilities.ConfigurationReader;
 import com.Odoo_Expenses.utilities.TestBase;
 import com.aventstack.extentreports.ExtentTest;
+import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
-public class ExpenseReportToApproveTest extends TestBase{
+public class ExpenseReportToApproveTest extends TestBase {
     @BeforeMethod
-    public void test1(){
-        extentLogger=  report.createTest("Login Test");
+    public void test1() {
+        extentLogger = report.createTest("Login Test");
         extentLogger.info("Sigin in logging page");
 
         pages.login().signIn();
@@ -21,43 +22,41 @@ public class ExpenseReportToApproveTest extends TestBase{
         //extentLogger.info("click on expenses button");
         pages.main().ExpensesButton.click();
 
-assertEquals(pages.main().ExpensesButton.getText(),
-        ApplicationConstants.MAIN_PAGE);
+        assertEquals(pages.main().ExpensesButton.getText(),
+                ApplicationConstants.MAIN_PAGE);
 
         extentLogger.info("main page is displayed");
         extentLogger.info(" test is passed");
 
-        WebDriverWait wait = new WebDriverWait(driver,5);
+        WebDriverWait wait = new WebDriverWait(driver, 5);
 
 
     }
 
 
-
     @Test
-    public void test2(){
+    public void test2() {
 
 
-        extentLogger=  report.createTest("expenses page Test");
+        extentLogger = report.createTest("expenses page Test");
 
-        WebDriverWait wait = new WebDriverWait(driver,5);
+        WebDriverWait wait = new WebDriverWait(driver, 5);
 
         extentLogger.info("Click on 'Expenses'. ");
 
         pages.expenseReportsToApprovePage().Expense();
 
 
-
         assertEquals(pages.expenseReportsToApprovePage()
 
-        .expense.getText(),ApplicationConstants.LIST_OF_ALL_PRODUCTS);
+                .expense.getText(), ApplicationConstants.LIST_OF_ALL_PRODUCTS);
 
 
         extentLogger.info("Expenses  is displayed");
 
         extentLogger.info(" Test passed");
 
-        extentLogger=  report.createTest
+        extentLogger = report.createTest
 
                 ("expenseReportsToApprovePage page Test");
 
@@ -67,7 +66,7 @@ assertEquals(pages.main().ExpensesButton.getText(),
 
         assertEquals(pages.expenseReportsToApprovePage()
 
-                .ExpenseReportsToApp.getText(),ApplicationConstants.EXPENSES_TO_REP);
+                .ExpenseReportsToApp.getText(), ApplicationConstants.EXPENSES_TO_REP);
 
         extentLogger.info(" expense Reports To ApprovePage is displayed");
 
@@ -75,6 +74,106 @@ assertEquals(pages.main().ExpensesButton.getText(),
 
     }
 
+    @Test
+    public void test3() {
+        //pages.login().signIn();
+        // pages.main().ExpensesButton.click();
+
+        pages.expensesPage().ExpenseReportsToApprove.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+
+        pages.expenseReportsToApprovePage().create.click();
+
+
+    }
+
+    @Test
+    public void test4() {
+
+
+        pages.expensesPage().ExpenseReportsToApprove.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+
+        pages.expenseReportsToApprovePage().create.click();
+
+        WebDriverWait wait1 = new WebDriverWait(driver, 15);
+
+pages.expenseReportsToApprovePage().Search.click();
+    }
+    @Test
+    public void test5(){
+        pages.expensesPage().ExpenseReportsToApprove.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+
+        pages.expenseReportsToApprovePage().create.click();
+
+        WebDriverWait wait1 = new WebDriverWait(driver, 15);
+
+        pages.expenseReportsToApprovePage().eployee.click();
+
+        WebDriverWait wait3 = new WebDriverWait(driver, 15);
+
+
+        pages.expenseReportsToApprovePage().summarybox.click();
+
+        pages.expenseReportsToApprovePage().
+
+                summarybox.sendKeys("expense for lunch");
+
+        WebDriverWait wait2 = new WebDriverWait(driver, 15);
+
+
+        pages.expenseReportsToApprovePage().approve.click();
+
+
+
+    }
+    @Test
+    public void test6(){
+        pages.expensesPage().ExpenseReportsToApprove.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+
+        pages.expenseReportsToApprovePage().create.click();
+
+        WebDriverWait wait1 = new WebDriverWait(driver, 15);
+
+        pages.expenseReportsToApprovePage().eployee.click();
+
+        WebDriverWait wait3 = new WebDriverWait(driver, 15);
+
+
+        pages.expenseReportsToApprovePage().summarybox.click();
+
+        pages.expenseReportsToApprovePage().
+
+                summarybox.sendKeys("expense for lunch");
+
+        WebDriverWait wait2 = new WebDriverWait(driver, 15);
+
+
+        pages.expenseReportsToApprovePage().refused.click();
+
+
+    }
+    @Test
+    public void test7(){
+        pages.expensesPage().ExpenseReportsToApprove.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+
+
+
+        //pages.expenseReportsToApprovePage().create.click();
+
+        pages.expenseReportsToApprovePage().Impo.click();
+    }
+
+
+
+
+
 }
-
-
